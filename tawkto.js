290f373c76909f210f449bbf5f112d165ba0bfaf
noerw <noerw@gmx.de>
2016-11-22 16:27:04 +0100

    add tawk.to integration
    TODO: create gitbook plugin that captures page changes

---
+++ tawkto.js
@@ -0,0 +1,9 @@
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/582d8dda1aad883390e8c575/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
